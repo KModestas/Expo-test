ebf12bf37a4b9dd652ba36f6d58c955a661b3b4b
Modestas <modestask97@gmail.com>
2018-08-10 12:49:32 +0100

    stack navigator login screen component

---
+++ screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class LoginScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>This is the login screen</Text>
+      </View>
+    );
+  }
+}

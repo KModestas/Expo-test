5c3c9425d7471087554b5ee3995058be90840cd1
Modestas <modestask97@gmail.com>
2018-08-10 13:17:23 +0100

    navigate button function that takes you from login screen to home screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button} from 'react-native';
 
 class LoginScreen extends Component {
   render() {
     return (
       <View>
         <Text>This is the login screen</Text>
+        <Button
+          onPress={()=> this.props.navigation.navigate('HomeScreen')}
+          title='Go to the HomeScreen' />
       </View>
     );
   }

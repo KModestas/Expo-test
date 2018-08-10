a6985a38d48b3b7e4cd328a1a60e1ddcc2b60757
Modestas <modestask97@gmail.com>
2018-08-10 12:54:55 +0100

    imported loginscreen and homescreen components in app.js

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
 
 export default class App extends React.Component {
   render() {
